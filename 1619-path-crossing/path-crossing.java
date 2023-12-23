675196227460311d55bf7ddf71b0c1a1b7e7cfb1
komalrathore1997 <86941235+komalrathore1997@users.noreply.github.com>
2023-12-23 11:13:02 +0530

    Time: 1 ms (96.48%) | Memory: 41.2 MB (52.64%) - LeetSync

--- 1619-path-crossing/path-crossing.java
+++ 1619-path-crossing/path-crossing.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean isPathCrossing(String path) {
+        Set<String> set=new HashSet<>();
+        set.add("0,0");
         int x=0;
         int y=0;
-        Map<Integer,HashSet<Integer>> map=new HashMap<>();
-        map.put(0,new HashSet<>());
-        map.get(0).add(0);
 
         for(int i=0;i<path.length();i++){
             char ch=path.charAt(i);
@@ -13,14 +12,13 @@ class Solution {
             else if(ch=='W') x--;
             else x++;
 
-            if(map.containsKey(x) && map.get(x).contains(y)){
-                return true;
-            }else if(map.containsKey(x)){
-                map.get(x).add(y);
-            }else{
-                map.put(x,new HashSet<>());
-                map.get(x).add(y);
-            }
+            StringBuilder sb=new StringBuilder();
+            sb.append(x).append(",").append(y);
+
+            if(set.contains(sb.toString()))
+               return true;
+
+            set.add(sb.toString());        
 
         }
 
