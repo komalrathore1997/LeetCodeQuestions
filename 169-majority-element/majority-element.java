1d4e49c06a977f05d24d623f92a764d36a1cf79d
komalrathore1997 <86941235+komalrathore1997@users.noreply.github.com>
2024-02-12 10:49:47 +0530

    Time: 1 ms (99.82%) | Memory: 50.3 MB (54.54%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+        int count=0;
+        int ans=0;
+
+        for(int num:nums){
+            if(count==0){
+                ans=num;
+            }
+
+            if(num==ans){
+              count++;
+            }else{
+              count--;
+            }
+        }
+
+        
+        return ans;
     }
+
 }
+
+// This is moore voting algorithm
